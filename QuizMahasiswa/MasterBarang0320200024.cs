3ff21ffee65b31c753cbb123c0b403827ca8ff4f
Roni Prasetyo <0320200024@polman.astra.ac.id>
2021-05-27 07:56:15 +0700

    Add project files.

---
+++ QuizMahasiswa/MasterBarang0320200024.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizMahasiswa
+{
+    public partial class MasterBarang0320200024 : Form
+    {
+        public MasterBarang0320200024()
+        {
+            InitializeComponent();
+        }
+    }
+}
